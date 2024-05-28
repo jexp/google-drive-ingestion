5b970d39404d148f5312ebd3c2b70ece68a36709
jacobbleakley-neo4j <jacob.bleakley@neo4j.com>
2024-05-28 15:43:38 +0100

    ChatGPT script iteration 1

--- drive_ingest.py
+++ drive_ingest.py
@@ -8,102 +8,87 @@ from googleapiclient.errors import HttpError
 from neo4j import GraphDatabase
 
 # If modifying these scopes, delete the file token.json.
-
-NEO4J_URI="bolt://localhost:7687"
-NEO4J_USERNAME="neo4j"
-NEO4J_PASSWORD="password"
-# Update the list of scopes to include the one for exporting files
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "password"
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 API_KEY = 'AIzaSyDeHlpiRM03MorwPk_fLmS-E3FkYG0HoVs'
 
-
 def main():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  # driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
-  kbFiles = None
+    driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+    kbFiles = None
+    exportFileList = []
 
-  try:
-    driveService = build("drive", "v3", credentials=creds, developerKey=API_KEY)
+    try:
+        driveService = build("drive", "v3", credentials=creds, developerKey=API_KEY)
+        kbFiles = (
+            driveService.files()
+            .list(
+                pageSize=1000,
+                supportsAllDrives=True,
+                includeItemsFromAllDrives=True,
+                includeTeamDriveItems=True,
+                supportsTeamDrives=True,
+                q="'1VyJnjVQxTA6PtvZUtzXI0liWz1Vb4SVk' in parents",
+                fields="nextPageToken, files(id, name, mimeType, parents, modifiedTime, shortcutDetails)"
+            )
+            .execute()
+        )
 
-    # Call the Drive v3 API
-    # This retrieves an object with all the files in the parent folder and their metadata
-    kbFiles = (
-        driveService.files()
-        .list(pageSize=1000, supportsAllDrives=True, includeItemsFromAllDrives=True, includeTeamDriveItems=True, supportsTeamDrives=True, q="'1VyJnjVQxTA6PtvZUtzXI0liWz1Vb4SVk' in parents", fields="nextPageToken, files(id, name, mimeType, parents, modifiedTime, shortcutDetails)")
-        .execute()
-    )
-    
-    print (kbFiles)
-    
-  except HttpError as error:
-    print(f"An error occurred: {error}") 
+    except HttpError as error:
+        print(f"An error occurred: {error}")
 
-  # initialise count and fileList
-  count = 0
-  fileList = []
-  
-  # Loop through the files and print their names and IDs
-  for file in kbFiles.get('files'):
-    count = count + 1
-    # fileContent = driveService.files().export(fileId=file['id'], mimeType='text/plain').execute().decode('utf-8')
-    # Check if the file is a shortcut
-    if file['mimeType'] == 'application/vnd.google-apps.shortcut':
-    # Fetch the target file ID from the shortcut
-      target_file_id = file.get('shortcutDetails', {}).get('targetId')
-      print("Target file ID: " + str(target_file_id))
-    
-      if target_file_id:
-        # Make a request to fetch the content of the target file
+    for file in kbFiles.get('files', []):
+        if file['mimeType'] == 'application/vnd.google-apps.shortcut':
+            target_file_id = file.get('shortcutDetails', {}).get('targetId')
+            target_file_name = file.get('shortcutDetails', {}).get('targetName')
+            if target_file_id and target_file_id not in exportFileList:
+                exportFileList.append({'id': target_file_id, 'name': target_file_name})
+        elif file['mimeType'] in ['application/vnd.google-apps.document', 'application/vnd.google-apps.presentation']:
+            if file['id'] not in exportFileList:
+                exportFileList.append({'id': file['id'], 'name': file['name']})
+
+    dbArrayList = []
+    for file_info in exportFileList:
         try:
-          target_file_request = driveService.files().export(fileId='1N8ts499ObXwoyZCvU-FA9K_zYRFx7yX7', mimeType='text/plain').execute().decode('utf-8')
-          print("Target file content: " + str(target_file_request))
+            fileContent = driveService.files().export(fileId=file_info['id'], mimeType='text/plain').execute().decode('utf-8')
+            dbArrayList.append({
+                'id': file_info['id'],
+                'name': file_info['name'],
+                'content': fileContent
+            })
         except HttpError as error:
-          if error.resp.status == 404:
-            print(f"File with ID {target_file_id} was not able to be retrieved")
-          else:
-            print(f"An error occurred while retrieving file with ID {target_file_id}: {error}")
-    else:
-    # If it's not a shortcut, fetch the content as usual
-      try:
-        if file['mimeType'] == 'application/vnd.google-apps.document' or file['mimeType'] == 'application/vnd.google-apps.presentation':
-          fileContent = driveService.files().export(fileId=file['id'], mimeType='text/plain', prettyPrint=True).execute().decode('utf-8')
-        else: 
-          fileContent = "Not a document or presentation"
-      except HttpError as error:
-          print(f"An error occurred while retrieving content for file with ID {file['id']}: {error}")
-          fileContent = None  # Set content to None if an error occurs
-    
-    nextFile = {"id":file['id'],"name":file['name'],"content":fileContent,"modifiedTime":file['modifiedTime']}
-    fileList.append(nextFile)
-  # # session = driver.session()
-  for file in fileList:
-    print(file)
-  # result = session.run("UNWIND $fileList as file MERGE (f:BrainFile:KB{id: file.id}) SET f.name = file.name MERGE (c:Content{content: file.content}) MERGE (f)-[:HAS_CONTENT]->(c) RETURN f.id, f.name, c.content",fileList=fileList)
-  # session.close()
-  
-  # driver.close()
-  
+            print(f"An error occurred while retrieving content for file with ID {file_info['id']}: {error}")
+
+    with driver.session() as session:
+        result = session.run(
+            "UNWIND $fileList as file "
+            "MERGE (f:BrainFile:KB {id: file.id}) "
+            "SET f.name = file.name "
+            "MERGE (c:Content {content: file.content}) "
+            "MERGE (f)-[:HAS_CONTENT]->(c) "
+            "RETURN f.id, f.name, c.content",
+            fileList=dbArrayList
+        )
+
+    driver.close()
+
 if __name__ == "__main__":
-  main()
+    main()
+
 
