e6bea6f6ed7555a2cb409530be49a5767cde5fff
jacobbleakley-neo4j <jacob.bleakley@neo4j.com>
2024-05-28 15:40:19 +0100

    Before testing chatGPT script

--- drive_ingest.py
+++ drive_ingest.py
@@ -14,7 +14,7 @@ NEO4J_USERNAME="neo4j"
 NEO4J_PASSWORD="password"
 # Update the list of scopes to include the one for exporting files
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-API_KEY = 'AIzaSyBHnAJ57OeSokJGnIKAY57hHKlwM7g9EWU'
+API_KEY = 'AIzaSyDeHlpiRM03MorwPk_fLmS-E3FkYG0HoVs'
 
 
 def main():
@@ -47,19 +47,23 @@ def main():
     driveService = build("drive", "v3", credentials=creds, developerKey=API_KEY)
 
     # Call the Drive v3 API
+    # This retrieves an object with all the files in the parent folder and their metadata
     kbFiles = (
         driveService.files()
         .list(pageSize=1000, supportsAllDrives=True, includeItemsFromAllDrives=True, includeTeamDriveItems=True, supportsTeamDrives=True, q="'1VyJnjVQxTA6PtvZUtzXI0liWz1Vb4SVk' in parents", fields="nextPageToken, files(id, name, mimeType, parents, modifiedTime, shortcutDetails)")
         .execute()
     )
     
+    print (kbFiles)
+    
   except HttpError as error:
     print(f"An error occurred: {error}") 
 
-  # print("No of pages: " + str(len(kbFiles.get('files'))))
-  # print("kbFiles: " + str(kbFiles))
+  # initialise count and fileList
   count = 0
   fileList = []
+  
+  # Loop through the files and print their names and IDs
   for file in kbFiles.get('files'):
     count = count + 1
     # fileContent = driveService.files().export(fileId=file['id'], mimeType='text/plain').execute().decode('utf-8')
@@ -92,7 +96,7 @@ def main():
     
     nextFile = {"id":file['id'],"name":file['name'],"content":fileContent,"modifiedTime":file['modifiedTime']}
     fileList.append(nextFile)
-  # session = driver.session()
+  # # session = driver.session()
   for file in fileList:
     print(file)
   # result = session.run("UNWIND $fileList as file MERGE (f:BrainFile:KB{id: file.id}) SET f.name = file.name MERGE (c:Content{content: file.content}) MERGE (f)-[:HAS_CONTENT]->(c) RETURN f.id, f.name, c.content",fileList=fileList)
